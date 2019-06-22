85f171353755a10c4af7c53d79796f671a774a65
Christoph Benjamin Weber <kriswep@wetainment.com>
2019-06-22 13:26:19 +0200

    use web navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBrowserApp } from '@react-navigation/web';
+import { Platform } from 'react-native';
 
 import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(
+const createApp = Platform.select({
+  web: config => createBrowserApp(config, { history: 'hash'}),
+  default: config => createAppContainer(config),
+})
+
+export default createApp(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
